3f89c65581beb42fc2c14f19d4185544506990c1
Unknown <milaneekhout@hotmail.nl>
2017-11-16 17:23:58 +0100

    beetje dingen aangedaan

--- site/index.php
+++ site/index.php
@@ -9,13 +9,14 @@ and open the template in the editor.
         <title>suk a dickcissel</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="favicon.ico">
+        <link href="menu_bar.css" rel="stylesheet" type="text/css"/>
     </head>
-    <body>
-        <?php
-            $bla = "bla";
-            echo "test";
-            echo $bla;
-        ?>
-        <p>hallo</p>
+    <body style="background-color: antiquewhite">
+         <a href="index.php">  
+            <img id="logo" src="logo.png" alt="homepage"/>
+        </a> 
+        <h1>GAMBLE SITE</h1>
+        <div id="points">points</div>
     </body>
 </html>

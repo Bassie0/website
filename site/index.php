d9513155b591457815a5f95a09545eb65bb673f4
Unknown <milaneekhout@hotmail.nl>
2017-11-18 18:39:20 +0100

    nog beter he

--- site/index.php
+++ site/index.php
@@ -1,30 +1,43 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <title>suk a dickcissel</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="favicon.ico">
-        <link href="menu_bar.css" rel="stylesheet" type="text/css"/>
+        <link href="css/menu_bar.css" rel="stylesheet" type="text/css"/>
+        <link href="css/maincontent.css" rel="stylesheet" type="text/css"/>
     </head>
-    <body style="background-color: #b38600">
-         <a href="index.php">  
-            <img id="logo" src="logo.png" alt="homepage"/>
+    <body style="background-color: grey">
+        <a id="logolink" href="index.php"> 
+            <div id="logobar"> 
+                <div id="logo"><img  src="logo.png" alt="homepage"></div>
+                <h1 id="logotext">GAMBLE SITE</h1>
+            </div> 
         </a> 
-        <h1>GAMBLE SITE</h1>
-        <div id="points">points</div>
-            <div id="menubar">
-                <li><a class="active" href="#home">Home</a></li>
-                <li><a href="slots">slots</a></li>
-                <li><a href="Info">Informie</a></li>
-                <li><a href=LoginButton">Log in</a></li>
-                <li><a href=myprofile">mijn profiel</a></li>
-            </div>
-
+        <div id="menubar">
+            <div id="menubutton"><a id="menubutton" href="#home">Home</a></div>
+            <div id="menubutton"><a id="menubutton" href="slots">slots</a></div>
+            <div id="menubutton"><a id="menubutton" href="Info">Informie</a></div>
+            <div id="menubutton"><a id="menubutton" href="inlog.php">Log in</a></div>
+            <div id="menubutton"><a id="menubutton" href="myprofile">mijn profiel</a></div>
+        </div>
+        <div id="maintekst">
+        <p id="main"> One morning, when Gregor Samsa woke from troubled 
+            dreams, he found himself transformed in his bed into 
+            a horrible vermin. He lay on his armour-like back, 
+            and if he lifted his head a little he could see his 
+            brown belly, slightly domed and divided by arches into 
+            stiff sections. The bedding was hardly able to cover 
+            <strong>strong</strong> it and seemed ready to slide 
+            off any moment. His many legs, pitifully thin 
+            compared with the size of the rest of him, 
+            <a class="external ext" href="#">link</a> waved about 
+            helplessly as he looked. "What's happened to me? " he 
+            thought. It was a dream. His room, a proper human 
+            room although a little too small, lay peacefully 
+            between its four familiar walls.
+        </p>
+        </div>
     </body>
 </html>
